9b7d31f70c0a465b3f4084aaa37eab8a5b909ba7
ghilger16 <ghilger16@yahoo.com>
2020-12-13 16:37:03 -0700

    day 3 second commit

--- script.js
+++ script.js
@@ -7,19 +7,33 @@ let itemList = document.getElementById("form-text");
 let btn = document.getElementById("btn");
 
 const addLi = function (text) {
+  /// user list item element
   let newItem = text;
-  let newLi = ul.appendChild(
-    document.createElement("LI").appendChild(document.createTextNode(text))
-      .parentNode
-  );
+  let newLi = document.createElement("LI");
+  let newText = document.createTextNode(`✏️ ${newItem}`);
+  newLi.setAttribute("id", "newLi");
+  newLi.appendChild(newText);
+  li.appendChild(newLi);
 
-  //   let newBtn = (newLi.appendChild(document.createElement("button")).innerText =
-  //     "done");
-  //   newBtn.addEventListener("click", function () {});
+  // item button element
+  let newBtn = document.createElement("button");
+  newBtn.innerText = "❌";
+  newBtn.setAttribute("id", "button");
+  newLi.appendChild(newBtn);
+
+  // list item event
+  newLi.addEventListener("click", () => {
+    newLi.style.textDecoration = "line-through";
+  });
+
+  // button event
+  newBtn.addEventListener("click", () => {
+    newLi.parentNode.removeChild(newLi);
+  });
 };
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
   let formText = new addLi(document.getElementById("form-text").value);
+  document.getElementById("form-text").value = "";
 });
-console.log(li.childNodes);
